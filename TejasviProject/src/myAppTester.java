94e8a92261715c34c499e804ac844f7962fadc87
Tejasvi Singh <singht29@student.douglascollege.ca>
2020-05-20 21:19:47 -0700

    Commited

---
+++ TejasviProject/src/myAppTester.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Minal Bhudia
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
